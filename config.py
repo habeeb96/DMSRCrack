47895cd756fb83fd6ab0b70552c9f6f755af1dec
Habeb Al-sameai <66535975+habeeb96@users.noreply.github.com>
2026-04-08 15:42:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,12 @@ import torch
 # ==========================================
 #             USER SETTINGS
 # ==========================================
+# GPU SETUP
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1" 
+
 # PATHS
-BASE_DIR = ''
+BASE_DIR = '/home/radhwan.saleh/surface_detection/datasets/crack1'
 TRAIN_IMG_DIR = os.path.join(BASE_DIR, 'train', 'images')
 TRAIN_MASK_DIR = os.path.join(BASE_DIR, 'train', 'masks')
 VAL_IMG_DIR = os.path.join(BASE_DIR, 'test', 'images')
@@ -14,7 +18,7 @@ VAL_MASK_DIR = os.path.join(BASE_DIR, 'test', 'masks')
 # HYPERPARAMETERS
 TARGET_CLASSES = ['DeepCrack'] 
 EPOCHS = 200
-BATCH_SIZE = 32       # Adjust based on VRAM
+BATCH_SIZE = 32       
 LEARNING_RATE = 3e-4  
 WEIGHT_DECAY = 1e-5
 
@@ -23,6 +27,6 @@ SAVE_NAME = "best_dmsrcrack_DeepCrack.pth"
 CSV_LOG_NAME = "training_metrics_DeepCrack.csv"
 
 # SYSTEM
-SEED = 42
+SEED = 1234
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 NUM_WORKERS = 4
